4b520c8f3fd2733123607bbb1d4b56dcc996deae
Juan Felipe <80426592+GomezJuanEfe@users.noreply.github.com>
2023-07-18 10:25:18 -0700

    Feature: header

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom"
+import Header from "../components/Header"
 
 const Root = () => {
 
   return (
     <>
-      <header>HEADER</header>
+      <Header/>
         <main>
           <Outlet />
         </main>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,12 @@
 import OurAwesomeFleet from "../components/OurAwesomeFleet"
 import OurHappyCustomer from "../components/OurHappyCustomer"
 import SuperEasy from "../components/SuperEasy"
-
 import WhyChooseRica from "../components/WhyChooseRica"
-
-
 
 const Home = () => {
 
   return (
     <>
-      <h2>Home</h2>
       <SuperEasy />
       <WhyChooseRica />
       <OurAwesomeFleet />
