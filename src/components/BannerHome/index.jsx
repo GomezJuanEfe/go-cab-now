2fbb2d0b0b8f769fb711aa48860af278bc49fd7a
Juan Felipe <80426592+GomezJuanEfe@users.noreply.github.com>
2023-07-19 09:18:18 -0700

    Feature: banner home
    * Navigation bar added
    
    * Footer done
    
    * banner home done

---
+++ src/components/BannerHome/index.jsx
@@ -0,0 +1,16 @@
+import './BannerHome.scss'
+
+const BannerHome = () => {
+
+  return (
+    <div className="banner-container">
+
+      <div className="banner-content">
+        <h1>Book Your Cab Now</h1>
+        <h3>Rent a Cab with afforable price with Go Cab Now</h3>
+      </div>
+    </div>
+  )
+}
+
+export default BannerHome
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,13 @@ import OurAwesomeFleet from "../components/OurAwesomeFleet"
 import OurHappyCustomer from "../components/OurHappyCustomer"
 import SuperEasy from "../components/SuperEasy"
 import WhyChooseRica from "../components/WhyChooseRica"
+import BannerHome from "../components/BannerHome"
 
 const Home = () => {
 
   return (
     <>
+      <BannerHome/>
       <SuperEasy />
       <WhyChooseRica />
       <OurAwesomeFleet />
