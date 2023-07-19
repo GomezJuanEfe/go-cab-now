46f7a53049c041417b1dc7685bb110420ffa60bf
ANDREA VARGAS <126208453+PVARGASM1@users.noreply.github.com>
2023-07-19 11:40:11 -0500

    Adding new component the best cab (#4)

--- src/components/SuperEasy/index.jsx
+++ src/components/SuperEasy/index.jsx
@@ -10,7 +10,7 @@ const SuperEasy = () => {
 return (
     <div>
       <section className='container__general' >
-            <h2>SUPER EASY</h2>
+            <h2>Super Easy Booking</h2>
             <p className='container__text'>Lorem Ipsum Dolor Sit Amet, Consectetur Adipisicing Elit.Adipisci 
             Alias Aperiam At, Aut Commodi Corporis Dolorum Ducimus Labore
             Magnam Mollitia Natus Porro Possimus Quae Sit Tenetur Veniam 
@@ -18,22 +18,22 @@ return (
             </p>
         <section className='container__icons'> 
 
-           <div className='card'>
+           <div className='se-card'>
               <img src={mobile}/>
               <p>Book Cab Threw Website Or App</p>
             </div>
 
-            <div className='card'>
+            <div id="card__down" className='se-card'>
               <img src={calendar}/>
               <p>Receive Confirmation</p>
             </div>
 
-            <div className='card'>
+            <div className='se-card'>
               <img src={taxi}/>
               <p>Meet Cab Driver On Pickup Time</p>
             </div>
 
-            <div className='card'>
+            <div id="card__down" className='se-card'>
               <img src={destination}/>
               <p>Arrive At Your Destination</p>
             </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import OurAwesomeFleet from "../components/OurAwesomeFleet"
 import OurHappyCustomer from "../components/OurHappyCustomer"
 import SuperEasy from "../components/SuperEasy"
 import WhyChooseRica from "../components/WhyChooseRica"
+import TheBestCab from "../components/TheBestCab"
 import BannerHome from "../components/BannerHome"
 
 const Home = () => {
@@ -9,9 +10,10 @@ const Home = () => {
   return (
     <>
       <BannerHome/>
-      <SuperEasy />
       <WhyChooseRica />
+      <SuperEasy />
       <OurAwesomeFleet />
+      <TheBestCab />
       <OurHappyCustomer />
     </>
   )
