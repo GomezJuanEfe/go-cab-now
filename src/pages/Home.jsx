0211804f93a0c05a0bf7195bfcfeaad1d534e8c5
PVARGASM1 <andrea.vargas18@hotmail.es>
2023-07-19 11:31:38 -0500

    Adding new component the best cab

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,16 @@ import OurAwesomeFleet from "../components/OurAwesomeFleet"
 import OurHappyCustomer from "../components/OurHappyCustomer"
 import SuperEasy from "../components/SuperEasy"
 import WhyChooseRica from "../components/WhyChooseRica"
+import TheBestCab from "../components/TheBestCab"
 
 const Home = () => {
 
   return (
     <>
-      <SuperEasy />
       <WhyChooseRica />
+      <SuperEasy />
       <OurAwesomeFleet />
+      <TheBestCab />
       <OurHappyCustomer />
     </>
   )
