6dd1e74c8175e2456cc2d5acf8e3d9bf0926be9e
Juan Gomez <jf.gomezramirez@gmail.com>
2023-07-22 15:50:21 -0500

    Mergin changes from remote

--- src/components/BannerHome/index.jsx
+++ src/components/BannerHome/index.jsx
@@ -1,9 +1,6 @@
 import './BannerHome.scss';
-<<<<<<< HEAD
 import DatePicker from '../DatePicker';
-=======
 import CabSearch from '../CabSearch';
->>>>>>> c632483032e5eb79bf5c9be371f7679bd3ee7cc3
 
 const BannerHome = () => (
   <div className="banner-container">
@@ -11,12 +8,9 @@ const BannerHome = () => (
     <div className="banner-content">
       <h1>Book Your Cab Now</h1>
       <h3>Rent a Cab with afforable price with Go Cab Now</h3>
-<<<<<<< HEAD
       <DatePicker />
       <DatePicker />
-=======
       <CabSearch />
->>>>>>> c632483032e5eb79bf5c9be371f7679bd3ee7cc3
     </div>
   </div>
 );
