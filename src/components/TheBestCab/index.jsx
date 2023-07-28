f268873adfb314063c61cc5afdacd26d6cab5a74
ANDREA VARGAS <126208453+PVARGASM1@users.noreply.github.com>
2023-07-28 10:45:48 -0500

    Feature thebest cab (#17)
    * Brand and best cab component
    
    * styles the best cab
    
    * Add medias queries and fixing the best cab component
    
    * Fixing Brands component
    
    * Changes medias queries
    
    * bug-fix: arrows deleted. Autoplay enabled
    
    ---------
    
    Co-authored-by: Juan Gomez <jf.gomezramirez@gmail.com>

--- src/components/TheBestCab/index.jsx
+++ src/components/TheBestCab/index.jsx
@@ -1,49 +1,52 @@
-import './TheBestCab.scss';
+/* eslint-disable no-undef */
 import img1 from '../../assets/images/3(1).png';
 import img2 from '../../assets/images/4(1).png';
+import './TheBestCab.scss';
+// eslint-disable-next-line import/order
+import { AiFillApple, AiFillAndroid } from 'react-icons/ai';
 
 const TheBestCab = () => (
   <div>
-    <section className="container__app">
-      <div className="app-content">
-        <span className="text__new">NEW</span>
-        <h2 className="title">
-          The best cab app
-          <br />
-          for your mobile app.
-        </h2>
-        <p>
-          Quisque sollicitudin feugiat risus, eu posuere ex euismod eu. Phasellus hendrerit, massa
-          efficitur dapibus pulvinar, sapien eros sodales ante, euismod aliquet nulla metus a
-          mauris.
-        </p>
-        <h3>Dowload App Now...</h3>
-        <div className="app__buttons">
-          <a href="https://www.apple.com/ios/app-store/">
-            <i
-              className="fab fa-apple"
-            />
-            {' '}
-            App Store
-          </a>
-          <a href="https://play.google.com/store?hl=en">
-            <i
-              className="fab fa-android"
-            />
-            {' '}
-            Play Store
-          </a>
+    <div className="limit__section">
+      <section className="container__app">
+        <div className="container_img">
+          <div className="image">
+            <img className="phones" src={img1} alt="phones1" />
+          </div>
+          <div className="image">
+            <img className="phones" src={img2} alt="phones2" />
+          </div>
         </div>
-      </div>
-      <div className="container_img">
-        <div className="image">
-          <img className="phones" src={img1} alt="phones1" />
+        <div className="app-content">
+          <div className="text__new">
+            <p className="button_new">NEW</p>
+          </div>
+          <h2 className="title">
+            The best cab app
+            <br />
+            for your mobile app.
+          </h2>
+          <p className="paragraph">
+            Quisque sollicitudin feugiat risus, eu posuere ex euismod eu. Phasellus hendrerit, massa
+            efficitur dapibus pulvinar, sapien eros sodales ante, euismod aliquet nulla metus a
+            mauris.
+          </p>
+          <h3>Dowload App Now...</h3>
+          <div className="app__buttons">
+            <a href="https://www.apple.com/ios/app-store/">
+              <AiFillApple style={{ fill: 'yellow', fontSize: '20px' }} />
+              {' '}
+              App Store
+            </a>
+            <a href="https://play.google.com/store?hl=en">
+              <AiFillAndroid style={{ fill: 'yellow', fontSize: '20px' }} />
+              {' '}
+              Play Store
+            </a>
+          </div>
         </div>
-        <div className="image">
-          <img className="phones" src={img2} alt="phones2" />
-        </div>
-      </div>
-    </section>
+      </section>
+    </div>
   </div>
 );
 
