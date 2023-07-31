c32b9e84ec1bdfc4cc3bfef5acc861cd561646e1
Juan Gomez <jf.gomezramirez@gmail.com>
2023-07-30 23:10:24 -0500

    Filter layout on Cab Search page done

---
+++ src/components/PriceSlider/index.jsx
@@ -0,0 +1,50 @@
+import './PriceSlider.scss';
+import { RangeSlider, createStyles, rem } from '@mantine/core';
+
+const valueLabelFormat = (number) => {
+  const options = {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 2,
+  };
+  return new Intl.NumberFormat('en-US', options).format(number);
+};
+
+const useStyles = createStyles((theme) => {
+  const yellow = '#e9b30e';
+  return ({
+    bar: {
+      backgroundColor: yellow,
+    },
+    thumb: {
+      height: rem(20),
+      borderColor: yellow,
+      backgroundColor: yellow,
+    },
+    label: {
+      fontWeight: '600',
+      backgroundColor: 'transparent',
+      color: 'black',
+      top: rem(-40),
+      fontSize: rem(20),
+    },
+  });
+});
+
+const PriceSlider = () => {
+  const { classes } = useStyles();
+  return (
+    <div className="slider-container">
+      <RangeSlider
+        size="md"
+        classNames={{ ...classes }}
+        labelAlwaysOn
+        label={valueLabelFormat}
+        max={1500}
+      />
+    </div>
+  );
+};
+
+export default PriceSlider;
