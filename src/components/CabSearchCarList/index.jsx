d57fb2c9209a907f1f1da19fb96858a4275ccfa9
Juan Felipe <80426592+GomezJuanEfe@users.noreply.github.com>
2023-08-12 10:28:34 -0500

    Feature/car data mock (#28)
    * Custom hooks implemented
    
    * useContext added
    
    * carData state set, cars context created
    
    * Cars context implemented, more cars data added, env variables implemented
    
    * Axios implemented
    
    * SWR implemented
    
    * Axios adapter implemented
    
    * pagination implemented, comentarios para desarrollar logica de paginación y filtro.
    
    * Pagination controls, object to query params function, request to backend function
    
    * Filter implemented
    
    * filter implemented on car type
    
    * filter implemented on car type
    
    * car-img to public folder

--- src/components/CabSearchCarList/index.jsx
+++ src/components/CabSearchCarList/index.jsx
@@ -1,23 +1,79 @@
-// import { useState } from 'react';
+import { useContext } from 'react';
 import './CabSearchCarList.scss';
 import CabSearchCarCard from '../CabSearchCarCard';
+import { CarsContext } from '../../store/CarsContext';
 
-const CabSearchCarList = () =>
-// const [carData, setCarData] = useState();
-(
-  <div className="cab-search-list">
-    <div className="list-container">
-      <CabSearchCarCard carName="Peugeot Citroen" carType="Sedan" />
-      <CabSearchCarCard carName="Susuki" carType="Medium" />
-      <CabSearchCarCard carName="Fiat Chrysler" carType="Mini" />
+const CabSearchCarList = () => {
+  const {
+    carsData, carsError, carsIsLoading, setCarPageIndex, carPageIndex, carPages,
+  } = useContext(CarsContext);
+  if (carsError) return <div>Failed to load</div>;
+  if (carsIsLoading) return <div>Loading...</div>;
+
+  const generatePagination = (pages) => (
+    Array.from({ length: pages }, (_, index) => index + 1)
+  );
+  return (
+    <div className="cab-search-list">
+      <div className="list-container">
+        {
+          carsData.result.map((item) => (
+            <CabSearchCarCard
+              key={item.car_id}
+              carName={item.car_name}
+              carType={item.car_type}
+              carImg={item.car_img}
+              carDetails={item.car_details}
+              fare={item.fare_km}
+            />
+          ))
+        }
+      </div>
+      <ul className="pagination">
+        {/* Previus arrow */}
+        {
+          carPages > 1
+            ? (
+              <li
+                className={`pagination__item ${carPageIndex === 1 ? 'disabled' : ''}`}
+                onClick={() => setCarPageIndex(carPageIndex - 1)}
+                onKeyDown={() => setCarPageIndex(carPageIndex - 1)}
+                role="menuitem"
+              >
+                «
+              </li>
+            ) : ''
+        }
+        {
+          generatePagination(carPages).map((index) => (
+            <li
+              key={index}
+              className={`pagination__item ${carPageIndex === index ? 'active' : ''}`}
+              onClick={() => setCarPageIndex(index)}
+              onKeyDown={() => setCarPageIndex(index)}
+              role="menuitem"
+            >
+              {index}
+            </li>
+          ))
+        }
+        {/* Next Arrow */}
+        {
+          carPages > 1
+            ? (
+              <li
+                className={`pagination__item ${carPageIndex === carPages ? 'disabled' : ''}`}
+                onClick={() => setCarPageIndex(carPageIndex + 1)}
+                onKeyDown={() => setCarPageIndex(carPageIndex + 1)}
+                role="menuitem"
+              >
+                »
+              </li>
+            )
+            : ''
+        }
+      </ul>
     </div>
-    <ul className="pagination">
-      <li className="pagination__item">«</li>
-      <li className="pagination__item active">1</li>
-      <li className="pagination__item">2</li>
-      <li className="pagination__item">3</li>
-      <li className="pagination__item">»</li>
-    </ul>
-  </div>
-);
+  );
+};
 export default CabSearchCarList;
