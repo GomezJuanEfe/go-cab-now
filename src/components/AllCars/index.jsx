58d46910e2d2fab78d7877410234d4988c19d26a
danielhincapievargas <daniel.hincapie.vargas@gmail.com>
2023-08-24 16:44:07 -0500

    AllCars component

--- src/components/AllCars/index.jsx
+++ src/components/AllCars/index.jsx
@@ -1,11 +1,51 @@
 import React from 'react';
+import { FaEdit } from 'react-icons/fa';
+import { AiOutlineDelete } from "react-icons/ai";
+import DashboardTitle from '../DashboardTableTitle';
+import DashboardTable from '../DashboardTable';
+import carsData from '../../services/Cars-data';
 
 const AllCars = () => {
-  const styles = {
-    marginLeft: '50px',
-  };
+  console.log('All cars');
   return (
-    <div style={styles}>All Cars</div>
+    <div className="table-container">
+      <DashboardTitle title="All Cars" />
+      <DashboardTable>
+        <table>
+          <thead>
+            <tr>
+              <th>Car Photo</th>
+              <th>Car Name</th>
+              <th>Type</th>
+              <th>Fare/Km</th>
+              <th>Edit</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {carsData.map((car, index) => (
+              <tr key={index}>
+                <td>
+                  <img src={car.car_photo} alt="car_photo" />
+                </td>
+                <td>{car.car_name}</td>
+                <td>{car.car_type}</td>
+                <td>
+                  $
+                  {car.fare}
+                </td>
+                <td>
+                  <FaEdit />
+                </td>
+                <td>
+                  <AiOutlineDelete />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </DashboardTable>
+    </div>
   );
 };
 
---
+++ src/services/Cars-data.js
@@ -0,0 +1,76 @@
+const carsData = [
+  {
+    car_photo: '../../public/cars/5.png',
+    car_name: 'Ford Fusion',
+    car_type: 'Sedan',
+    fare: 25,
+  },
+  {
+    car_photo: '../../public/cars/5.png',
+    car_name: 'Ford Fusion',
+    car_type: 'Sedan',
+    fare: 25,
+  },
+  {
+    car_photo: '../../public/cars/5.png',
+    car_name: 'Ford Fusion',
+    car_type: 'Sedan',
+    fare: 25,
+  },
+  {
+    car_photo: '../../public/cars/5.png',
+    car_name: 'Ford Fusion',
+    car_type: 'Sedan',
+    fare: 25,
+  },
+  {
+    car_photo: '../../public/cars/5.png',
+    car_name: 'Ford Fusion',
+    car_type: 'Sedan',
+    fare: 25,
+  },
+  {
+    car_photo: '../../public/cars/5.png',
+    car_name: 'Ford Fusion',
+    car_type: 'Sedan',
+    fare: 25,
+  },
+  {
+    car_photo: '../../public/cars/5.png',
+    car_name: 'Ford Fusion',
+    car_type: 'Sedan',
+    fare: 25,
+  },
+  {
+    car_photo: '../../public/cars/5.png',
+    car_name: 'Ford Fusion',
+    car_type: 'Sedan',
+    fare: 25,
+  },
+  {
+    car_photo: '../../public/cars/5.png',
+    car_name: 'Ford Fusion',
+    car_type: 'Sedan',
+    fare: 25,
+  },
+  {
+    car_photo: '../../public/cars/5.png',
+    car_name: 'Ford Fusion',
+    car_type: 'Sedan',
+    fare: 25,
+  },
+  {
+    car_photo: '../../public/cars/5.png',
+    car_name: 'Ford Fusion',
+    car_type: 'Sedan',
+    fare: 25,
+  },
+  {
+    car_photo: '../../public/cars/5.png',
+    car_name: 'Ford Fusion',
+    car_type: 'Sedan',
+    fare: 25,
+  },
+];
+
+export default carsData;
