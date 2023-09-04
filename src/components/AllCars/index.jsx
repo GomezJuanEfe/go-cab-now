f9d9f519c285b91949799a98629bb0e1f5d02a1b
ANDREA VARGAS <126208453+PVARGASM1@users.noreply.github.com>
2023-09-04 17:32:48 -0500

    Feature/cars integration (#42)
    * Carousel added to the OurAwesomeFleet component
    
    * primer bugfix de errores con Eslint
    
    * AllCars component
    
    * Bookings integration
    
    * integration
    
    * integration CreateTrips almost done
    
    * Integration GETALLCARS to allcars component
    
    * Start add new car
    
    * Adding styles and edit-car component
    
    * Adding integration delete
    
    * Adding modal in allcars component funcionality delete almost done
    
    * Adding integration delete and edit not finished
    
    * FInish delete integration
    
    * Working in update integration
    
    * Integration byId and edit cars
    
    * Renderizado condicional All Cars
    
    * Setiando statado actual en formEditCar
    
    * Styles added
    
    * All user view done
    
    * Condition about role DRIVER and ADMIN
    
    ---------
    
    Co-authored-by: danielhincapievargas <daniel.hincapie.vargas@gmail.com>
    Co-authored-by: Juan Gomez <jf.gomezramirez@gmail.com>

--- src/components/AllCars/index.jsx
+++ src/components/AllCars/index.jsx
@@ -1,11 +1,139 @@
-import React from 'react';
+import { useContext, useEffect, useState } from 'react';
+import './AllCars.scss';
+import axios from 'axios';
+import { FaEdit } from 'react-icons/fa';
+import { AiOutlineDelete } from 'react-icons/ai';
+import { useNavigate } from 'react-router-dom';
+import DashboardTitle from '../DashboardTableTitle';
+import DashboardTable from '../DashboardTable';
+import Modal from '../Modal';
+import ModalCarInformation from '../ModalCarInformation';
+import { DashboardContext } from '../../store/DashboardContext';
+import { UserContext } from '../../store/UserContext';
+
+const URL = import.meta.env.VITE_API_URL;
 
 const AllCars = () => {
-  const styles = {
-    marginLeft: '50px',
+  const {
+    dataCars, setDataCars, selectedCar, setSelectCar,
+  } = useContext(DashboardContext);
+  const { userData } = useContext(UserContext);
+
+  const [modalDelete, setModalDelete] = useState(false);
+  const [deleteCar, setDeleteCar] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchCarsData = async () => {
+      setLoading(true);
+
+      try {
+        const { data: { cars } } = await axios.get((userData.role === 'ADMIN' ? `${URL}/api/cars` : `${URL}/api/cars/single`), {
+          headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+        });
+        if (userData.role === 'ADMIN') {
+          setDataCars(cars);
+        } else {
+          setDataCars([cars]);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+      setLoading(false);
+    };
+
+    fetchCarsData();
+  }, []);
+
+  const handleShowModalCar = (carInformation) => {
+    setSelectCar({
+      id: carInformation.id,
+      img: carInformation.img,
+      car_name: carInformation.car_name,
+      type: carInformation.type,
+      fare_km: carInformation.fare_km,
+    });
+
+    setModalDelete(true);
   };
+
+  const navigate = useNavigate();
+
+  const handleEditCar = (item) => {
+    setSelectCar(item);
+    navigate('/user-profile/edit-car');
+  };
+
   return (
-    <div style={styles}>All Cars</div>
+    <>
+      <div className="table-container">
+        <DashboardTitle title="Cars" />
+        <DashboardTable>
+          <table>
+            <thead>
+              <tr>
+                <th>Car Photo</th>
+                <th>Car Name</th>
+                <th>Type</th>
+                <th>Fare/Km</th>
+                <th>Edit</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {!loading && dataCars?.map((car, index) => (
+                <tr key={index}>
+                  <td>
+                    <img src={car.img} alt="car" />
+                  </td>
+                  <td>{car.car_name}</td>
+                  <td>{car.type}</td>
+                  <td>
+                    $
+                    {car.fare_km}
+                  </td>
+
+                  <td>
+                    <FaEdit
+                      onClick={() => handleEditCar(car)}
+                      className="icon_edit"
+                    />
+                  </td>
+
+                  <td>
+                    <AiOutlineDelete
+                      onClick={() => handleShowModalCar(car)}
+                      className="icon_delete"
+                    />
+                  </td>
+
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </DashboardTable>
+      </div>
+
+      <Modal
+        showModal={modalDelete}
+        handleShowModal={() => setModalDelete(false)}
+      >
+        <ModalCarInformation
+          selectedCar={selectedCar} // Pasa la info car selected
+          setModalDelete={setModalDelete}
+          setDeleteCar={setDeleteCar}
+        />
+      </Modal>
+
+      <Modal
+        showModal={deleteCar}
+        handleShowModal={() => setDeleteCar(false)}
+      >
+        <div className="center">
+          <h1>Your car was deleted successfully</h1>
+        </div>
+      </Modal>
+    </>
   );
 };
 
