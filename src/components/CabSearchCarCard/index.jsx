bd2610f50e6ee6e001638ce367b6ddf5f3631ded
danielhincapievargas <daniel.hincapie.vargas@gmail.com>
2023-09-07 13:00:48 -0500

    Trip total on CabSearchCarCard

--- src/components/CabSearchCarCard/index.jsx
+++ src/components/CabSearchCarCard/index.jsx
@@ -1,51 +1,60 @@
 import './CabSearchCarCard.scss';
+import { useContext } from 'react';
+import { FormContext } from '../../store/FormContext';
 import { usdFormat, toTitleCase } from '../../services/utils';
+import distanceBetweenCities from '../../services/citiesDistance';
 
-const CabSearchCarCard = ({ data, handleSelect }) => (
-  <div className="cab-search-card">
-    <div className="card__header">
-      <div className="card__img">
-        <img src={data.img} alt={`${data.car_name} car`} />
+const CabSearchCarCard = ({ data, handleSelect }) => {
+  const { tripForm } = useContext(FormContext);
+  return (
+    <div className="cab-search-card">
+      <div className="card__header">
+        <div className="card__img">
+          <img src={data.img} alt={`${data.car_name} car`} />
+        </div>
+        <h6>{data.car_name}</h6>
+        <p>{`(${data.type})`}</p>
       </div>
-      <h6>{data.car_name}</h6>
-      <p>{`(${data.type})`}</p>
+      <div className="card__details">
+        <ul>
+          <li>
+            <span className="seat-icon" />
+            {`${data.seats} seats`}
+          </li>
+          {data.air_conditioner
+            ? (
+              <li>
+                <span className="ac-icon" />
+                AC
+              </li>
+            )
+            : <li />}
+          <li>
+            <span className="luggage-icon" />
+            {`${data.luggage} luggage`}
+          </li>
+          <li>
+            <span className="trans-icon" />
+            {toTitleCase(data.transmition)}
+          </li>
+        </ul>
+      </div>
+      <div className="card__price">
+        <h3>
+          {usdFormat((distanceBetweenCities(tripForm.pickUpLoc, tripForm.dropOffLoc))
+         * data.fare_km * 100)}
+        </h3>
+        <p>
+          Fare/km:
+          {' '}
+          <span>{`$${data.fare_km}`}</span>
+        </p>
+      </div>
+      <div className="card__button">
+        <button className="secondary-button" type="button" onClick={handleSelect}>Book Now</button>
+      </div>
     </div>
-    <div className="card__details">
-      <ul>
-        <li>
-          <span className="seat-icon" />
-          {`${data.seats} seats`}
-        </li>
-        {data.air_conditioner
-          ? (
-            <li>
-              <span className="ac-icon" />
-              AC
-            </li>
-          )
-          : <li />}
-        <li>
-          <span className="luggage-icon" />
-          {`${data.luggage} luggage`}
-        </li>
-        <li>
-          <span className="trans-icon" />
-          {toTitleCase(data.transmition)}
-        </li>
-      </ul>
-    </div>
-    <div className="card__price">
-      <h3>{usdFormat(120000)}</h3>
-      <p>
-        Fare/km:
-        {' '}
-        <span>{`$${data.fare_km}`}</span>
-      </p>
-    </div>
-    <div className="card__button">
-      <button className="secondary-button" type="button" onClick={handleSelect}>Book Now</button>
-    </div>
-  </div>
-);
+  );
+};
 
 export default CabSearchCarCard;
--- src/components/LocationPicker/index.jsx
+++ src/components/LocationPicker/index.jsx
@@ -4,11 +4,33 @@ const LocationPicker = ({
   title, inpName, placeholder, modal, handleInput, value,
 }) => {
   const modalStyle = modal ? 'modal' : '';
+  const cities = [
+    'Bogotá',
+    'Medellín',
+    'Cali',
+    'Barranquilla',
+    'Cartagena',
+    'Bucaramanga',
+    'Santa Marta',
+    'Pereira',
+    'Manizales',
+    'Cúcuta',
+  ];
   return (
     <div className={`label-input ${modalStyle}`}>
       <label htmlFor={inpName}>
         <div>{title}</div>
-        <input type="text" id={inpName} placeholder={placeholder} name={inpName} onChange={(e) => handleInput(e)} value={value} />
+        <select
+          id={inpName}
+          placeholder={placeholder}
+          name={inpName}
+          onChange={(e) => handleInput(e)}
+          value={value}
+          required
+        >
+          <option value="" disabled>Choose a city</option>
+          {cities.map((city) => (<option key={city} value={city}>{city}</option>))}
+        </select>
       </label>
     </div>
   );
