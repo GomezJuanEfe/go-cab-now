a376e3deff4af381c7b95be61c53883b564eff10
danielhincapievargas <daniel.hincapie.vargas@gmail.com>
2023-09-14 15:50:18 -0500

    Date and Location Verification Modals

--- src/components/CabSearchCarList/index.jsx
+++ src/components/CabSearchCarList/index.jsx
@@ -1,15 +1,18 @@
 import './CabSearchCarList.scss';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Pagination from '../Pagination';
 import { CarContext } from '../../store/CarContext';
 import CabSearchCarCard from '../CabSearchCarCard';
 import { FormContext } from '../../store/FormContext';
+import Modal from '../Modal';
 import { usdFormat } from '../../services/utils';
 import distanceBetweenCities from '../../services/citiesDistance';
 
 const CabSearchCarList = () => {
   const { tripForm } = useContext(FormContext);
+  const [locationAlert, setLocationAlert] = useState(false);
+  const [dateAlert, setDateAlert] = useState(false);
   const {
     data,
     error,
@@ -27,12 +30,26 @@ const CabSearchCarList = () => {
     const totalPrice = usdFormat((distanceBetweenCities(tripForm.pickUpLoc, tripForm.dropOffLoc))
     * item.fare_km * 100);
     setSelectedCarPrice(totalPrice);
+    if (tripForm.pickUpLoc === '' || tripForm.dropOffLoc === '' || tripForm.pickUpLoc === tripForm.dropOffLoc) {
+      setLocationAlert(true);
+      return;
+    } if (tripForm.pickUpDate === '') {
+      setDateAlert(true);
+      return;
+    }
     navigate('/cab-booking');
     window.scroll({ top: '0', behavior: 'smooth' });
   };
 
   if (error) return <div>Failed to load</div>;
   if (isLoading) return <div>Loading...</div>;
+
+  const handleShowLocationAlert = () => {
+    setLocationAlert(!locationAlert);
+  };
+  const handleShowDateAlert = () => {
+    setDateAlert(!dateAlert);
+  };
 
   return (
     <div className="cab-search-list" id="main">
@@ -54,6 +71,18 @@ const CabSearchCarList = () => {
         pageIndex={cablistIndexPage}
         setPageIndex={setcablistIndexPage}
       />
+      <Modal showModal={locationAlert} handleShowModal={handleShowLocationAlert}>
+        <h2>Must select Pick Up and Drop Off Locations</h2>
+        <div className="center">
+          <button type="button" className="secondary-button" onClick={() => setLocationAlert(false)}>Ok</button>
+        </div>
+      </Modal>
+      <Modal showModal={dateAlert} handleShowModal={handleShowDateAlert}>
+        <h2>Must select a Pick Up Date and Time</h2>
+        <div className="center">
+          <button type="button" className="secondary-button" onClick={() => setDateAlert(false)}>Ok</button>
+        </div>
+      </Modal>
     </div>
   );
 };
