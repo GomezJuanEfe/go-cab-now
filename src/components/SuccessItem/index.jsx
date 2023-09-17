569a61b1bd7170ed9d57ec149d44f0561ba53f21
danielhincapievargas <daniel.hincapie.vargas@gmail.com>
2023-09-17 12:29:52 -0500

    payment page info done

--- src/components/SuccessItem/index.jsx
+++ src/components/SuccessItem/index.jsx
@@ -2,22 +2,44 @@ import './SuccessItem.scss';
 import ImgCar from '../../assets/images/success_img.png';
 
 // eslint-disable-next-line arrow-body-style
-const SuccessItem = ({ title, body, children }) => {
-  return (
-    <section className="success-section">
-      <div>
-        <div className="row_success">
-          <div className="col_success">
-            <img src={ImgCar} alt="successCar" />
-            <h2>{title}</h2>
-            <p>
-              {body}
-            </p>
-            {children}
-          </div>
+const SuccessItem = ({
+  title, origin, destination, date, total, firstName, lastName, email, children,
+}) => (
+  <section className="success-section">
+    <div>
+      <div className="row_success">
+        <div className="col_success">
+          <img src={ImgCar} alt="successCar" />
+          <h2>{title}</h2>
+          <table>
+            <tbody>
+              <tr>
+                <td>Booking holder</td>
+                <td>{`${firstName} ${lastName}`}</td>
+              </tr>
+              <tr>
+                <td>Origin</td>
+                <td>{origin}</td>
+              </tr>
+              <tr>
+                <td>Destination</td>
+                <td>{destination}</td>
+              </tr>
+              <tr>
+                <td>Pick up date</td>
+                <td>{date}</td>
+              </tr>
+              <tr>
+                <td>Total charges</td>
+                <td>{total}</td>
+              </tr>
+            </tbody>
+          </table>
+          <p>{`Booking confirmation was sent to ${email}`}</p>
+          {children}
         </div>
       </div>
-    </section>
-  );
-};
+    </div>
+  </section>
+);
 export default SuccessItem;
--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -1,19 +1,41 @@
 /* eslint-disable import/no-named-as-default-member */
 /* eslint-disable import/no-named-as-default */
 /* eslint-disable linebreak-style */
+import React, { useContext } from 'react';
 import BannerPages from '../components/BannerPages';
 import SuccessItem from '../components/SuccessItem';
+import { FormContext } from '../store/FormContext';
+import { CarContext } from '../store/CarContext';
+import { formatTableDate } from '../services/DateFormat';
+import { useNavigate } from 'react-router-dom';
 
-const Success = () => (
-  <div>
-    <BannerPages pageName="Booking Success" />
-    <SuccessItem
-      title="Payment Successful ! get ready to ride"
-      body="Thank You For You Payment. We Have Received Your Payment Successfully. Your Transaction ID Is 'SHJG12155215', You Will Get An Email Invoice Soon!"
-    >
-      <button type="submit" className="secondary-button">Download Invoice</button>
-    </SuccessItem>
-  </div>
-);
+const Success = () => {
+  const { tripForm, contactForm } = useContext(FormContext);
+  const { selectedCarPrice } = useContext(CarContext);
+  const navigate = useNavigate();
+
+  const handleSubmit = () => {
+    navigate('/');
+    window.scroll({ top: '0', behavior: 'smooth' });
+  };
+
+  return (
+    <div>
+      <BannerPages pageName="Booking Success" />
+      <SuccessItem
+        title="Payment Successful! Thanks for your booking!"
+        origin={tripForm.pickUpLoc}
+        destination={tripForm.dropOffLoc}
+        date={formatTableDate(tripForm.pickUpDate)}
+        total={selectedCarPrice}
+        firstName={contactForm.firstName}
+        lastName={contactForm.lastName}
+        email={contactForm.emailAddress}
+      >
+        <button type="button" onClick={handleSubmit} className="secondary-button">Go Cab Now</button>
+      </SuccessItem>
+    </div>
+  );
+};
 
 export default Success;
